ab3897c18acf37ca394a1a7262c6ac57a04bafba
Brian Wong <brianwong@zipcoders-MBP.fios-router.home>
2019-03-09 00:11:09 -0500

    Finished Duplicate Deleter

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,46 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Stream<Integer> duplicateRemoved = Arrays.stream(array).filter(integer ->
+                counter(integer) < maxNumberOfDuplications);
+        Integer[] arrayWithoutDups = duplicateRemoved.toArray(Integer[]::new);
+        return  arrayWithoutDups;
+
+    }
+
+    public Integer counter(Integer integer) {
+        Integer counter = 0;
+        for (Integer number :array) {
+            if(number.equals(integer)){
+                counter++;
+            }
+        }
+        return  counter;
+    }
+
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Stream<Integer> duplicateRemoved = Arrays.stream(array).filter(integer ->
+                counter(integer) != exactNumberOfDuplications);
+        Integer[] arrayWithoutDups = duplicateRemoved.toArray(Integer[]::new);
+        return  arrayWithoutDups;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,37 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        Stream<String> stringStream = Arrays.stream(array).filter(string -> counter(string) < maxNumberOfDuplications);
+        String[] arrayWithoutDuplicates = stringStream.toArray(String[]::new);
+        return arrayWithoutDuplicates;
+    }
+
+    public Integer counter(String string) {
+        int counter = 0;
+        for (String word: array) {
+            if(word.equals(string)) {
+                counter++;
+            }
+        } return counter;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Stream<String> stringStream = Arrays.stream(array).filter(string -> counter(string) != exactNumberOfDuplications);
+        String[] arrayWithoutDuplicates = stringStream.toArray(String[]::new);
+        return arrayWithoutDuplicates;
+    }
 }
